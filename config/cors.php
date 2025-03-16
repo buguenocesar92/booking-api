5381b0913ada51a1f4e35e414b424d3f56a6babb
buguenocesar92 <buguenocesar92@gmail.com>
2025-03-16 16:21:09 -0300

    Fix CORS configuration by correcting syntax error in allowed_origins array

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     // Especifica el origen exacto de tu frontend
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:5173'. 'https://gilded-gelato-8c2ec1.netlify.app'],
+    'allowed_origins' => ['http://localhost:3000', 'http://localhost:5173', 'https://gilded-gelato-8c2ec1.netlify.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],

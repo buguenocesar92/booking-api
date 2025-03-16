71bc14ea9fb3b4887419e3ff3a941caf4e5e7da2
buguenocesar92 <buguenocesar92@gmail.com>
2025-03-16 00:03:33 -0300

    Update CORS configuration to restrict allowed origins and remove auth paths

--- config/cors.php
+++ config/cors.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'auth/*'],  // Asegúrate de incluir los endpoints que usas
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'], // O ['*'] para desarrollo, pero cuidado en producción
+    'allowed_origins' => ['https://gilded-gelato-8c2ec1.netlify.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
